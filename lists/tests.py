bdb1e3bf08d7acaff7366332246e24e5af76dbc3
nunchuck <nicholas.ball@sainsburys.co.uk>
2017-09-25 14:38:40 +0100

    Commit to revert

--- lists/tests.py
+++ lists/tests.py
@@ -6,7 +6,26 @@ class HomePageTest(TestCase):
     class ListViewTest(TestCase):
 
 
+<<<<<<< HEAD
         class ListAndItemModelsTest(TestCase):
+=======
+    def test_can_save_a_POST_request(self):
+        self.client.post('/', data={'item_text': 'A new list item'})
+
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, 'A new list item')
+
+    def test_redirects_after_POST(self):
+        response = self.client.post('/', data={'item_text': 'A new list item'})
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response['location'], '/lists/the-only-list-in-the-world/')
+
+    def test_only_saves_items_when_necessary(self):
+        self.client.get('/')
+        self.assertEqual(Item.objects.count(), 0)
+
+>>>>>>> parent of 36ad968... URLs for forms x2, moved code in views + tests, new URL
 
             def test_saving_and_retrieving_items(self):
                 list_ = List()
@@ -48,6 +67,7 @@ class HomePageTest(TestCase):
 
                     response = self.client.get('/lists/the-only-list-in-the-world/')
 
+<<<<<<< HEAD
                     self.assertContains(response, 'itemey 1')
                     self.assertContains(response, 'itemey 2')
 
@@ -62,3 +82,7 @@ class HomePageTest(TestCase):
             def test_redirects_after_POST(self):
                 response = self.client.post('/lists/new', data={'item_text': 'A new list item'})
                 self.assertRedirects(response, '/lists/the-only-list-in-the-world/')
+=======
+            self.assertContains(response, 'itemey 1')
+            self.assertContains(response, 'itemey 2')
+>>>>>>> parent of 36ad968... URLs for forms x2, moved code in views + tests, new URL
