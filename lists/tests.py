6e981358ddc2f1c908d524753934868347019653
nunchuck <nicholas.ball@sainsburys.co.uk>
2017-09-25 14:40:40 +0100

    Commit to revert

--- lists/tests.py
+++ lists/tests.py
@@ -7,10 +7,29 @@ class HomePageTest(TestCase):
 
 
 <<<<<<< HEAD
+<<<<<<< HEAD
         class ListAndItemModelsTest(TestCase):
 =======
     def test_can_save_a_POST_request(self):
         self.client.post('/', data={'item_text': 'A new list item'})
+=======
+    def test_can_save_a_POST_request(self):
+        self.client.post('/', data={'item_text': 'A new list item'})
+
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, 'A new list item')
+
+    def test_redirects_after_POST(self):
+        response = self.client.post('/', data={'item_text': 'A new list item'})
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response['location'], '/lists/the-only-list-in-the-world/')
+
+    def test_only_saves_items_when_necessary(self):
+        self.client.get('/')
+        self.assertEqual(Item.objects.count(), 0)
+
+>>>>>>> parent of 36ad968... URLs for forms x2, moved code in views + tests, new URL
 
         self.assertEqual(Item.objects.count(), 1)
         new_item = Item.objects.first()
@@ -54,6 +73,7 @@ class HomePageTest(TestCase):
                 self.assertEqual(second_saved_item.text, 'Item the second')
                 self.assertEqual(second_saved_item.list, list_)
 
+<<<<<<< HEAD
         class ListViewTest(TestCase):
 
             def test_uses_list_template(self):
@@ -82,6 +102,10 @@ class HomePageTest(TestCase):
             def test_redirects_after_POST(self):
                 response = self.client.post('/lists/new', data={'item_text': 'A new list item'})
                 self.assertRedirects(response, '/lists/the-only-list-in-the-world/')
+=======
+            self.assertContains(response, 'itemey 1')
+            self.assertContains(response, 'itemey 2')
+>>>>>>> parent of 36ad968... URLs for forms x2, moved code in views + tests, new URL
 =======
             self.assertContains(response, 'itemey 1')
             self.assertContains(response, 'itemey 2')
--- lists/views.py
+++ lists/views.py
@@ -14,11 +14,14 @@ def view_list(request):
     items = Item.objects.all()
     return render(request, 'list.html', {'items' : items})
 <<<<<<< HEAD
+<<<<<<< HEAD
 
 
 def new_list(request):
     list_ = List.objects.create()
     Item.objects.create(text=request.POST['item_text'])
     return redirect('/lists/the-only-list-in-the-world/')
+=======
+>>>>>>> parent of 36ad968... URLs for forms x2, moved code in views + tests, new URL
 =======
 >>>>>>> parent of 36ad968... URLs for forms x2, moved code in views + tests, new URL
